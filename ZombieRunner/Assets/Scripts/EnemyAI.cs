b9c958b45474529530b0a7a799b14eea792dff39
Sarvottam <sarvottamswaroop100@gmail.com>
2022-10-29 15:10:41 +0530

    Navmesh Agent Added

---
+++ ZombieRunner/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyAI : MonoBehaviour
+{
+    
+    [SerializeField] Transform target; 
+    NavMeshAgent navMeshAgent;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();   
+    }
+
+    void Update()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
